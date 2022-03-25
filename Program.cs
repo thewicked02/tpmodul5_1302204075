c618ddd021c809a51a3fc14121d5d00300efc811
rodo <rodonatanael02@gmail.com>
2022-03-25 17:03:08 +0700

    menambahkan class SayTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine();
+﻿
+namespace tpmod5_1302204075
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             SayaTubeVideo s = new SayaTubeVideo("Tutorial Design By Contract - RODO IDOLA NATHANAEL MARBUN - 1302204075");
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod5_1302204075
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int PlayCount;
+        public SayaTubeVideo(String title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.PlayCount = 0;
+        }
+
+        public void increasePlayCount(int a) { PlayCount++; }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine(this.id);
+            Console.WriteLine(this.title);
+            Console.WriteLine(this.PlayCount);
+        }
+
+    }
+}
